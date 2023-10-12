c78f60b5a48e8f05b49bbe2098ef78f2a3ff3c3c
LeMehdy <98973915+LeMehdy@users.noreply.github.com>
2023-10-13 00:12:50 +0200

    Add the database

---
+++ DatabasePOPCORN.sql
@@ -0,0 +1,87 @@
+INSERT INTO Movies (MovieID, ReleaseDate, Director, Description, MovieUrlPicture, MovieDuration)
+VALUES (1, '2021-07-10', 'Christopher Nolan', 'Inception is a mind-bending thriller.', 'inception.jpg', 148);
+
+INSERT INTO Movies (MovieID, ReleaseDate, Director, Description, MovieUrlPicture, MovieDuration)
+VALUES (2, '2019-05-15', 'Quentin Tarantino', 'Once Upon a Time in Hollywood is a nostalgic drama.', 'hollywood.jpg', 161);
+
+INSERT INTO Movies (MovieID, ReleaseDate, Director, Description, MovieUrlPicture, MovieDuration)
+VALUES (3, '2020-02-20', 'Greta Gerwig', 'Little Women is a classic novel adaptation.', 'littlewomen.jpg', 135);
+
+INSERT INTO Movies (MovieID, ReleaseDate, Director, Description, MovieUrlPicture, MovieDuration)
+VALUES (4, '2022-09-05', 'Denis Villeneuve', 'Dune is an epic sci-fi adventure.', 'dune.jpg', 155);
+
+INSERT INTO Movies (MovieID, ReleaseDate, Director, Description, MovieUrlPicture, MovieDuration)
+VALUES (5, '2018-12-25', 'Ryan Coogler', 'Black Panther is a superhero film set in Wakanda.', 'blackpanther.jpg', 134);
+
+INSERT INTO Movies (MovieID, ReleaseDate, Director, Description, MovieUrlPicture, MovieDuration)
+VALUES (6, '2019-03-08', 'Bong Joon-ho', 'Parasite is a dark comedy about class struggle.', 'parasite.jpg', 132);
+
+INSERT INTO Movies (MovieID, ReleaseDate, Director, Description, MovieUrlPicture, MovieDuration)
+VALUES (7, '2021-11-03', 'Martin Scorsese', 'The Irishman is a crime epic.', 'irishman.jpg', 209);
+
+INSERT INTO Movies (MovieID, ReleaseDate, Director, Description, MovieUrlPicture, MovieDuration)
+VALUES (8, '2020-07-03', 'Spike Lee', 'Da 5 Bloods is a war drama.', 'da5bloods.jpg', 154);
+
+INSERT INTO Movies (MovieID, ReleaseDate, Director, Description, MovieUrlPicture, MovieDuration)
+VALUES (9, '2019-11-27', 'Taika Waititi', 'Jojo Rabbit is a satirical war film.', 'jojorabbit.jpg', 108);
+
+INSERT INTO Movies (MovieID, ReleaseDate, Director, Description, MovieUrlPicture, MovieDuration)
+VALUES (10, '2022-04-14', 'Jordan Peele', 'Nope is a mysterious horror movie.', 'nope.jpg', 130);
+INSERT INTO Actor (ActorID, ActorLastName, ActorFirstName, ActorNationality, ActorUrlPicture)
+VALUES ('A1', 'DiCaprio', 'Leonardo', 'American', 'leo.jpg');
+
+INSERT INTO Actor (ActorID, ActorLastName, ActorFirstName, ActorNationality, ActorUrlPicture)
+VALUES ('A2', 'Roberts', 'Julia', 'American', 'julia.jpg');
+
+INSERT INTO Actor (ActorID, ActorLastName, ActorFirstName, ActorNationality, ActorUrlPicture)
+VALUES ('A3', 'Hanks', 'Tom', 'American', 'tom.jpg');
+
+INSERT INTO Actor (ActorID, ActorLastName, ActorFirstName, ActorNationality, ActorUrlPicture)
+VALUES ('A4', 'Blunt', 'Emily', 'British', 'emily.jpg');
+
+INSERT INTO Actor (ActorID, ActorLastName, ActorFirstName, ActorNationality, ActorUrlPicture)
+VALUES ('A5', 'Smith', 'Will', 'American', 'will.jpg');
+
+INSERT INTO Actor (ActorID, ActorLastName, ActorFirstName, ActorNationality, ActorUrlPicture)
+VALUES ('A6', 'Kidman', 'Nicole', 'Australian', 'nicole.jpg');
+
+INSERT INTO Actor (ActorID, ActorLastName, ActorFirstName, ActorNationality, ActorUrlPicture)
+VALUES ('A7', 'Cruise', 'Tom', 'American', 'tomcruise.jpg');
+
+INSERT INTO Actor (ActorID, ActorLastName, ActorFirstName, ActorNationality, ActorUrlPicture)
+VALUES ('A8', 'Johansson', 'Scarlett', 'American', 'scarlett.jpg');
+
+INSERT INTO Actor (ActorID, ActorLastName, ActorFirstName, ActorNationality, ActorUrlPicture)
+VALUES ('A9', 'Hemsworth', 'Chris', 'Australian', 'chris.jpg');
+
+INSERT INTO Actor (ActorID, ActorLastName, ActorFirstName, ActorNationality, ActorUrlPicture)
+VALUES ('A10', 'Lawrence', 'Jennifer', 'American', 'jennifer.jpg');
+INSERT INTO Genre (GenreID, GenreName)
+VALUES (1, 'Action');
+
+INSERT INTO Genre (GenreID, GenreName)
+VALUES (2, 'Drama');
+
+INSERT INTO Genre (GenreID, GenreName)
+VALUES (3, 'Sci-Fi');
+
+INSERT INTO Genre (GenreID, GenreName)
+VALUES (4, 'Comedy');
+
+INSERT INTO Genre (GenreID, GenreName)
+VALUES (5, 'Crime');
+
+INSERT INTO Genre (GenreID, GenreName)
+VALUES (6, 'War');
+
+INSERT INTO Genre (GenreID, GenreName)
+VALUES (7, 'Horror');
+
+INSERT INTO Genre (GenreID, GenreName)
+VALUES (8, 'Mystery');
+
+INSERT INTO Genre (GenreID, GenreName)
+VALUES (9, 'Biography');
+
+INSERT INTO Genre (GenreID, GenreName)
+VALUES (10, 'Adventure');
