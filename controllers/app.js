b0f156edba031910ebe23f447ce1ac598621b0ae
LeMehdy <mehdy.michalak@efrei.net>
2023-11-21 14:08:03 +0100

    Updating code
    adding log in/ sign up features

---
+++ controllers/app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const axios = require('axios');
+const registerRouter = require('./register');
+
+const apiKey = 'df1e67f93440369e82c54d553192cb3b'; 
+
+app.set("view engine", "ejs");
+app.set('views', path.join( './views'));
+app.use("/img", express.static("img/"))
+app.use('/register', registerRouter);
+app.get('/movie/:id', async (req, res) => {
+    try {
+        const movieId = req.params.id;
+        // Appel à l'API pour récupérer les détails du film en utilisant l'ID du film
+        const response = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`);
+        const movieDetails = response.data;
+
+        // Rendre la vue (template) EJS en transmettant les détails du film
+        res.render('movie-details', { movieDetails: movieDetails });
+    } catch (error) {
+        res.status(500).json({ error: 'Erreur lors de la récupération des détails du film' });
+    }
+});
+
+app.get('/', async (req, res) => {
+    try {
+        const trendingResponse = await axios.get(`https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}`);
+        const trendingMovies = trendingResponse.data.results;
+
+        const newMoviesResponse = await axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=fr-FR&page=1`);
+        const newMovies = newMoviesResponse.data.results;
+
+        res.render('index', { movies: trendingMovies, newMovies: newMovies });
+    } catch (error) {
+        res.status(500).json({ error: 'Erreur lors de la récupération des données' });
+    }
+});
+
+// Écoutez le port pour les requêtes HTTP
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Serveur en cours d'exécution sur le port ${port}`);
+});
---
+++ controllers/register.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require("mysql");
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env' });
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+db.connect((error) => {
+    if (error) {
+        console.error('Erreur de connexion à la base de données :', error);
+    } else {
+        console.log('MySQL connecté avec succès !');
+    }
+});
+
+// Page d'inscription (register)
+router.get('/', (req, res) => {
+    res.render('register'); // Ou tout autre traitement pour cette route
+});
+
+// Traitement du formulaire d'inscription
+router.post('/', (req, res) => {
+    const { username, email, password, full_name, date_of_birth, bio, profile_image_url } = req.body;
+
+    const user = {
+        username,
+        email,
+        password,
+        full_name,
+        date_of_birth,
+        bio,
+        profile_image_url
+    };
+
+    db.query('INSERT INTO users SET ?', user, (err, result) => {
+        if (err) {
+            res.status(500).json({ error: 'Erreur lors de l\'inscription' });
+            throw err;
+        }
+        res.redirect('/login'); // Redirection vers la page de connexion après l'inscription
+    });
+});
+
+module.exports = router;
